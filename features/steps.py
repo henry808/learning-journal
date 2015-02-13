3a35c1ebcee87b8e792a268fa694a49b7276fd39
Henry Grantham <hgrantham@gmail.com>
2015-02-13 13:43:40 -0800

    latest version

--- features/steps.py
+++ features/steps.py
@@ -38,19 +38,22 @@ TEST_DSN = 'dbname=test_learning_journal user=JustinKan'
 #     assert world.url == '/'
 
 
-@step(u'Given an entry with      print(\'([^\']*)\')')
+@step(u'Given an entry with      print("Text")')
 def given_an_entry_with_print_group1(step, group1):
-    assert False, 'This step must be implemented'
+    world.make_entry("Title", '    print("Text")')
 
 
 @step(u'When I go to the detail page with id2')
 def when_i_go_to_the_detail_page_with_id2(step):
-    assert False, 'This step must be implemented'
+    response = world.app.get('/')
+    response.click(href='detail/2')
+    assert response.status_code == 200
 
 
 @step(u'Then I will see the tag for .codehilite')
 def then_i_will_see_the_tag_for_codehilite(step):
-    assert False, 'This step must be implemented'
+    response = world.app.get('/detail/2')
+    assert 'class="codehilite"' in response.body
 
 
 @step(u'Given the edit page and id of 1')
@@ -107,15 +110,22 @@ def teardown_db(total):
         db.commit()
 
 
+@world.absorb
+def make_entry(title, text):
+    data = {'title': title, 'text': text}
+    response = world.app.post('/add', params=data, status='3*')
+    return response
 
 @before.each_scenario
-def make_entry(scenario):
+def make_init_entry(scenario):
     """mock a request with a database attached"""
     now = datetime.datetime.utcnow()
     expected = ('Test Title', 'Test Text', now)
     with closing(connect_db(world.settings)) as db:
         run_query(db, INSERT_ENTRY, expected, False)
         db.commit()
+
+    world.make_entry('Test Title', 'Test Text')
 
 
 @after.each_scenario
