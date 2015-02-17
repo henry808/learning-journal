0db12d3222d07e225aa7fce2ce75a99353cc4b2c
Henry Grantham <hgrantham@gmail.com>
2015-02-16 17:04:38 -0800

    added tests for colorize, markdown, and permalink and changed some features for same

--- features/markdown.feature
+++ features/markdown.feature
@@ -2,6 +2,6 @@ Feature: MarkDown
     Implement a way to MarkDown an entry to be able to make pretty
 
     Scenario: Entry text contains MarkDown language
-        Given Entry has text with '# Header'
+        Given Entry has text with "# Header"
         When I go to the detail page of id 2
         Then I see a H1 tag
--- features/steps.py
+++ features/steps.py
@@ -1,40 +1,35 @@
-import datetime
 import os
-# from lettuce import *
 from lettuce import step
 from lettuce import world
 from lettuce import before
 from lettuce import after
-# from journal import *
 from journal import main
-from journal import INSERT_ENTRY
-from test_journal import run_query
-# from journal import edit_entry
 
 from webtest import TestApp
 
 from journal import connect_db
 from journal import DB_SCHEMA
-from cryptacular.bcrypt import BCRYPTPasswordManager
 from contextlib import closing
 
 TEST_DSN = 'dbname=test_learning_journal user=JustinKan'
 
 
 # FEATURE: COLORIZE
-@step(u"Given an entry with ```X = Y```")
+@step(u'Given an entry with colorized code')
 def given_an_entry_with_print_group1(step):
-    assert False, 'This step must be implemented'
+    world.make_entry('Colorize Title', '```python \nX = Y\n```')
 
 
-@step(u'When I go to the detail page with id2')
-def when_i_go_to_the_detail_page_with_id2(step):
-    assert False, 'This step must be implemented'
+@step(u'When I go to the detail page with id3')
+def when_i_go_to_the_detail_page_with_id3(step):
+    world.id = 2
+    world.response = world.app.get('/detail/' + str(world.id))
+    assert world.response.status_code == 200
 
 
-@step(u'Then I will see the tag for .codehilite')
+@step(u'Then I will see the tag for codehilite')
 def then_i_will_see_the_tag_for_codehilite(step):
-    assert False, 'This step must be implemented'
+    assert "codehilite" in world.response.body
 
 
 # FEATURE: EDIT ENTRY
@@ -50,7 +45,6 @@ def when_i_finish_loading_the_edit_page_the_url_is_edit_1(step):
     assert '<h2>Edit</h2>' in world.response.body
     assert 'Test Text' in world.response.body
     # add something that checks the url later (pull this info out of headers)
-    # assert world.response.  == '/edit/1'
 
 
 @step(u'Then I see the text for id 1 in the text box')
@@ -76,15 +70,7 @@ def change_text_and_submit(step):
     world.response.form['text'] = 'Edit now'
     # check to make sure text contains 'Edit now' after
     assert world.response.form['text'].value == 'Edit now'
-
     world.response = world.response.form.submit()
-
-    # used these lines for debugging:
-
-    # print world.response
-    # print "form: " + str(world.response.form.fields.values())
-    # print "form: " + str(world.response.form.method)
-    # print "body: " + str(world.response.body)
 
 
 @step(u"Then I see 'Edited now' in homepage")
@@ -92,7 +78,6 @@ def edit_shows_up(step):
     # check to see if we were redirected
     assert world.response.status_code == 302
     # check to see if we are in homepage (not written)
-
     world.response = world.app.get('/')
     assert 'My Python Journal' in world.response.body
     # check to see if 'Edited now' is in homepage
@@ -116,74 +101,38 @@ def then_i_see_the_homepage(step):
 
 
 # FEATURE: MARKDOWN
-@step(u'Given Entry has text with \'([^\']*)\'')
-def given_entry_has_text_with_group1(step, group1):
-    assert False, 'This step must be implemented'
+@step(u'Given Entry has text with "# Header"')
+def given_entry_has_text_with_group1(step):
+    world.make_entry('Markdown Title', '# Header')
 
 
 @step(u'When I go to the detail page of id 2')
 def when_i_go_to_the_detail_page_of_id_2(step):
-    assert False, 'This step must be implemented'
+    world.id = 3
+    world.response = world.app.get('/detail/' + str(world.id))
+    assert world.response.status_code == 200
 
 
 @step(u'Then I see a H1 tag')
 def then_i_see_a_h1_tag(step):
-    assert False, 'This step must be implemented'
+    assert "<h1>Header</h1>" in world.response.body
 
 
 # FEATURE: PERMALINK
 @step(u'Given entry with id 1')
 def given_entry_with_id_1(step):
-    assert False, 'This step must be implemented'
+    world.id = 1
 
 
 @step(u'When I go to the url')
 def when_i_go_to_the_url(step):
-    assert False, 'This step must be implemented'
+    world.response = world.app.get('/detail/' + str(world.id))
 
 
 @step(u'Then I see that it is detail/1 and the text for id 1 displays')
 def then_i_see_that_it_is_detail_1_and_the_text_for_id_1_displays(step):
-    assert False, 'This step must be implemented'
-
-
-
-
-
-# @step('given authorized user and homepage')
-# def authorized_edit_entry(step):
-#     world.authorized = True
-
-
-# @step('When on homepage')
-# def on_homepage(step):
-#     world.url = "http://127.0..0.1:5000/"
-
-
-# @step('Then I will see buttons')
-# def check_for_button(step):
-#     assert world.url == '/'
-
-
-# @step(u'Given an entry with      print("Text")')
-# def given_an_entry_with_print_group1(step, group1):
-#     assert True
-#     # world.make_entry("Title", '    print("Text")')
-
-
-# @step(u'When I go to the detail page with id2')
-# def when_i_go_to_the_detail_page_with_id2(step):
-#     # response = world.app.get('/')
-#     # response.click(href='detail/2')
-#     assert True
-#     # assert response.status_code == 200
-
-
-# @step(u'Then I will see the tag for .codehilite')
-# def then_i_will_see_the_tag_for_codehilite(step):
-#     # response = world.app.get('/detail/2')
-#     assert True
-#     # assert 'class="codehilite"' in response.body
+    # check to see if 'Edited now' is in the detail page
+    assert 'Edit now' in world.response.body
 
 
 @before.all
@@ -221,21 +170,9 @@ def make_entry(title, text):
     response = world.app.post('/add', params=data, status='3*')
     return response
 
+    # used these lines for debugging:
 
-# @before.each_scenario
-# def make_init_entry(scenario):
-#     """mock a request with a database attached"""
-#     now = datetime.datetime.utcnow()
-#     expected = ('Test Title', 'Test Text', now)
-#     with closing(connect_db(world.settings)) as db:
-#         run_query(db, INSERT_ENTRY, expected, False)
-#         db.commit()
-
-    # world.make_entry('Test Title 2', 'Test Text')
-
-
-# @after.each_scenario
-# def clear_entries_after(scenario):
-#     with closing(connect_db(world.settings)) as db:
-#         db.cursor().execute("DELETE FROM entries")
-#         db.commit()
+    # print world.response
+    # print "form: " + str(world.response.form.fields.values())
+    # print "form: " + str(world.response.form.method)
+    # print "body: " + str(world.response.body)
